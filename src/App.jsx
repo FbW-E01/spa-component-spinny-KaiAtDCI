24a6c8ac3cc9fd80615e55783b4090c62b8abeb5
KaiAtDCI <kaiatdci@web.de>
2021-07-19 13:48:32 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {useEffect, useState} from "react";
+import Spinner from "./Spinner";
+import './App.css';
+
+export default function App() {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users?_delay=5000&t='+Math.random())
+            .then(response => response.json())
+            .then(result => setUsers(result))
+    }, []);
+
+    return (
+        <div className="App">
+            { users.length === 0
+                ?   <Spinner/>
+                :   <div>
+                    {users.map((user, key) => <div key={key}>{user.name}</div>)}
+                </div>
+            }
+        </div>
+    );
+}
---
+++ src/Spinner.jsx
@@ -0,0 +1,10 @@
+import './Spinner.css';
+
+export default function Spinner() {
+    return (
+        <div className="Spinner">
+            <i className="fas fa-spinner"></i>
+        </div>
+    );
+}
+
